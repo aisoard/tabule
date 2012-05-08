77de562f709aa4a4d7f3b21abb2cfd9be7f5d764
Alexandre Isoard <alexandre.isoard@gmail.com>
2012-05-08 21:28:53 +0200

    Use icu utf8 macro

--- src/utf8.cpp
+++ src/utf8.cpp
@@ -1,14 +1,14 @@
 #include "utf8.h"
 
 #include <string>
+#include <unicode/utf8.h>
 
 namespace std {
 	int utf8_length(string const utf8_string) {
 		int count = 0;
 
 		for(auto i = utf8_string.begin(); i != utf8_string.end(); i++) {
-			unsigned char k = *i;
-			if(0b00100000 <= k && k < 0b10000000 || 0b11000000 <= k)
+			if(U8_IS_SINGLE(*i) || U8_IS_LEAD(*i))
 				count++;
 		}
 
